101cb945ed2d4834c7a94ef6dded644b66722950
Paul Watson <paulewiii3@gmail.com>
2022-02-24 22:19:58 -0500

    stringifyJSON Complete

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,38 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+  var result = '';
+
+  if (Array.isArray(obj)) {
+    result += '[';
+    for (var i = 0; i < obj.length; i++) {
+      result += stringifyJSON(obj[i]) + ',';
+      if (i === obj.length - 1) {
+        result = result.slice(0, -1);
+      }
+    }
+    result += ']';
+  } else if (typeof obj === 'object' && obj !== null) {
+    result += '{';
+    var keys = Object.keys(obj);
+    for (var i = 0; i < keys.length; i++) {
+      if (typeof obj[keys[i]] === 'function' || obj[keys[i]] === undefined) {
+        continue;
+      }
+      result += '"' + keys[i] + '":';
+      result += stringifyJSON(obj[keys[i]]) + ',';
+      if (i === keys.length - 1) {
+        result = result.slice(0, -1);
+      }
+    }
+    result += '}';
+  } else if (typeof obj === 'string') {
+    result += '"' + obj + '"';
+  } else if (typeof obj === 'number') {
+    result += '' + obj;
+  } else {
+    result += String(obj);
+  }
+  console.log(result);
+  return result;
 };
